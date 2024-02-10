4d2ce60b863ecac24cdc3c1aa0846b52abc2c80a
[gggalvinoTI] <[glaucegomesgalvino@gmail.com]>
2024-02-10 19:27:54 -0300

    TerceiraParte

--- Exercicio.py
+++ Exercicio.py
@@ -7,9 +7,36 @@ cursor = conexao.cursor()
 
 #2. Insira pelo menos 5 registros de alunos na tabela que você criou no exercício anterior
 #cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(1,"Eduardo Gomes","25","Administração" )')
-cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(2,"Camila Oliveira","27","Psicologia" )')
-cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(3,"Ellen Fernandez","23","Direito" )')
-cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(4,"Talita Vieira","21","Nutrição" )')
-cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(5,"Leonardo Nogueira","28","Matemática" )')
+#cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(2,"Camila Oliveira","27","Psicologia" )')
+#cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(3,"Ellen Fernandez","23","Direito" )')
+#cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(4,"Talita Vieira","21","Nutrição" )')
+#cursor.execute('INSERT INTO Alunos(Id,Nome,Idade,Curso) VALUES(5,"Leonardo Nogueira","28","Matemática" )')
+
+#3. Consultas Básicas
+#Escreva consultas SQL para realizar as seguintes tarefas:
+#a) Selecionar todos os registros da tabela "alunos".
+dados = cursor.execute('SELECT * FROM alunos')
+for alunos in dados:
+    print (alunos)
+
+#b) Selecionar o nome e a idade dos alunos com mais de 20 anos.
+dados = cursor.execute('SELECT nome,idade FROM alunos where Idade >20')
+for alunos in dados:
+    print (alunos)
+
+#alterar curso dos aluno para engenharia
+cursor.execute('UPDATE alunos SET curso = "Engenharia" WHERE nome = "Talita Vieira"') 
+cursor.execute('UPDATE alunos SET curso = "Engenharia" WHERE nome = "Ellen Fernandez"') 
+#c) Selecionar os alunos do curso de "Engenharia" em ordem alfabética.
+dados = cursor.execute('SELECT * FROM alunos WHERE curso = "Engenharia" ORDER BY nome ASC')
+for aluno in dados:
+    print(aluno)
+
+#d) Contar o número total de alunos na tabela
+
+cursor.execute('SELECT COUNT(*) FROM alunos')
+total_alunos = cursor.fetchone()[0]
+print("Total de alunos:", total_alunos)
+    
 conexao.commit()
 conexao.close()
